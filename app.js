998834b9635850ec701e2a35ebc594ea1cea81de
___techbros <94129364+Michael-Aladejobi@users.noreply.github.com>
2024-05-06 15:17:13 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+count = 0;
+
+function triggerIncrement(){
+    count += 1 ;
+    document.getElementById('count-el').innerText = count;
+}
+
+function triggerDecrement(){
+    count -= 1;
+    document.getElementById('count-el').innerText = count;
+}
+
+function triggerReset(){
+    count = 0;
+    document.getElementById('count-el').innerText = count;
+}
